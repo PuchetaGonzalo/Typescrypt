06c3f673e5045b68776a5ed9d0fae29fa10b748f
Gonzalo Pucheta <91892387+PuchetaGonzalo@users.noreply.github.com>
2022-02-25 11:29:36 -0300

    Delete seccion3 directory

--- seccion3/function.ts
+++
@@ -1,9 +0,0 @@
-function sumar(a:number,b:number) :number {
-    return a + b
-}
-
-const suma:number = sumar(6,2)
-const suma2:number = sumar(8,3)
-const suma3:number = sumar(16,20)
-
-console.log(suma3)
